15a336b734061351bf3407b5d94581043224888c
alonakechilz <49635962+alonakechilz@users.noreply.github.com>
2026-04-29 22:57:37 +0700

    Create MainActivity with empty onCreate method
    Initial implementation of MainActivity with onCreate method.

---
+++ MainActivity.java
@@ -0,0 +1,11 @@
+package com.ipank.loader;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Kita biarkan kosong dulu biar robotnya ngerakit cepet
+    }
+}
