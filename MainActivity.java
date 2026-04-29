fab32b96e95d8c420b25b41d52a3592417933d13
alonakechilz <49635962+alonakechilz@users.noreply.github.com>
2026-04-29 23:41:27 +0700

    Remove comment in onCreate method
    Removed comment to allow faster robot assembly.

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,5 @@
 package com.ipank.loader;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -6,6 +7,5 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Kita biarkan kosong dulu biar robotnya ngerakit cepet
     }
 }
